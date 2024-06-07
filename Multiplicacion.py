334d0f02d3241a61e077a49d9c9daccc6e2d4903
Bruno-Canales <brun.canales@duocuc.cl>
2024-06-07 19:50:31 -0400

    agregue hola

--- Multiplicacion.py
+++ Multiplicacion.py
@@ -2,3 +2,4 @@ num_1=int(input("Ingrese numero 1: "))
 num_2=int(input("Ingrese numero 2: "))
 multiplicacion=num_1*num_2
 print("La multiplicacion entre",num_1, "y", num_2,"es:",multiplicacion )
+print("HOLA")
